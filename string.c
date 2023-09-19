65270a49d9b4b94ebfa0032d27ecebb6cabd83e8
Grace Nwakeze <112550367+Unique081@users.noreply.github.com>
2023-09-19 20:26:03 +0100

    Add files via upload
    Printf project with Patrichy

---
+++ string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *string_length - for thr specifier for string character
+  *
+  *@s: the string entered
+  *
+  *Return: print the string out
+  */
+int string_length(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
